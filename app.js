9dac42ae6b559f95438076d92795803fd4e2a137
Sandeep Shukla <sandeepini.2012@gmail.com>
2020-07-30 15:06:43 +0530

    Second Commit Simple Template for marketing or informational website

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-	res.send('<h1>Hello World!! , Here I use express</h1>');
+	res.render('index',{title: 'Welcome'});
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+app.get('/contact', function(req, res){
+	res.render('contact');
 });
 
 app.listen(3000);
